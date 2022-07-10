5b05ef939ab1e1f6ebd931f8ab0f9cf9a2223ad1
GBN6 <czesiox8@gmail.com>
2022-07-10 18:44:13 +0200

    Add basic task factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+import createNewTask from "./task";
+
+const task1 = createNewTask('Clean up', 'CLEAN THIS MESS IN KITCHEN', '12.21.2222', 'HIGH', 0, 0,)
+
+// console.log(tasks.myTasks[0])
+
+console.log(task1.projectIndex);
---
+++ src/task.js
@@ -0,0 +1,15 @@
+const createNewTask = (title, info, date, priority, projectIndex, taskIndex) => {
+    return{
+        title,
+        info, 
+        date, 
+        priority, 
+        projectIndex, 
+        taskIndex
+    }
+}
+
+
+
+
+export default createNewTask;
