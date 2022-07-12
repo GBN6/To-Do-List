cc2fd921808dccbe7493836f2850327fa953e4a5
GBN6 <czesiox8@gmail.com>
2022-07-12 19:24:54 +0200

    Create and implement to the DOM Project List
    I've added project class and interface for user
    to create his own project that will be displayed
    on the nav bar, projects can be deleted, still working
    on setting active project and adding tasks

--- main.js
+++ main.js
@@ -7,100 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _task__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task */ \"./src/task.js\");\n/* harmony import */ var _project__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./project */ \"./src/project.js\");\n\n\n\n\nlet test = new _task__WEBPACK_IMPORTED_MODULE_0__[\"default\"](\"Try This\", \"Attempt to make a todo\", new Date().toDateString(), \"High\", true);\n\nlet test2 = new _project__WEBPACK_IMPORTED_MODULE_1__[\"default\"]('New Project');\n\ntest2.addTask(\"Try This\", \"Attempt to make a todo\", new Date().toDateString(), \"High\", true);\nconsole.log(test2);\n\n\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/project.js":
-/*!************************!*\
-  !*** ./src/project.js ***!
-  \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _task__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task */ \"./src/task.js\");\n\n\nclass Project {\n    constructor(title){\n        this.title = title;\n        this.tasksList = [];\n    }\n\n    addTask(title, info, dueDate, priority) { \n        this.tasksList.push(new _task__WEBPACK_IMPORTED_MODULE_0__[\"default\"](title, info, dueDate, priority));\n    }\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Project);\n\n//# sourceURL=webpack://to-do-list/./src/project.js?");
-
-/***/ }),
-
-/***/ "./src/task.js":
-/*!*********************!*\
-  !*** ./src/task.js ***!
-  \*********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nclass Tasks {\n    constructor(title, info, dueDate, priority) {\n        this.title = title;\n        this.info = info;\n        this.dueDate = dueDate;\n        this.priority = priority;\n        this.completed = false;\n    }\n\n    toggleCompleted() {\n        this.completed === false ? this.completed = true : this.completed = false;\n    }\n\n    editTask(key, value) {\n        this[key] = value;\n    }\n}\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Tasks);\n\n//# sourceURL=webpack://to-do-list/./src/task.js?");
+eval("//     DOM\nconst createProject = (() => {\n    setActiveProject();\n    const createNewProjectButton = document.querySelector('.btn-project-user');\n\n    createNewProjectButton.addEventListener('click', displayProjectInput)\n\n    function displayProjectInput() {\n        const newProjectPopup = document.querySelector('.add-project-popup');\n        const addNewProject = document.querySelector('.btn-add-project-popup');\n\n        addNewProject.addEventListener('click', addProject)\n        newProjectPopup.classList.add('active');\n        createNewProjectButton.classList.add('active');\n        closeProjectInput();\n    }\n\n    function closeProjectInput() {\n        const newProjectPopup = document.querySelector('.add-project-popup');\n        const addNewProject = document.querySelector('.btn-add-project-popup');\n        const closeProject = document.querySelector('.btn-close-poject-popup');\n        addNewProject.addEventListener('click', () => {\n            createNewProjectButton.classList.remove('active');\n            newProjectPopup.classList.remove('active')\n        })\n        closeProject.addEventListener('click', () => {\n            createNewProjectButton.classList.remove('active');\n            newProjectPopup.classList.remove('active')\n        })\n    }\n\n    function addProject() {\n        const newProjectInput = document.querySelector('.input-add-project-popup');\n        const inputError = document.querySelector('.title-error')\n        let projectName = newProjectInput.value.trim();\n        if (projectName === '') {\n            inputError.textContent = 'Project must have name'\n            return;\n        }\n        if (projectList.contains(projectName)) {\n            inputError.textContent = 'Project must have different names'\n            return;\n        }\n        projectList.addProject(projectName);\n        inputError.textContent = '';\n        clearUserProjects()\n        renderProjectList();\n        newProjectInput.value = '';\n    }\n\n    function renderProjectList() {\n        projectList.forEach(project => {\n                if (\n                    project.title !== 'Your Tasks' &&\n                    project.title !== 'Today' && \n                    project.title !== 'This week' \n                ) {\n                    renderProjectsNav(project.title);\n                }\n            })\n    }\n\n    function deleteProject() {\n        const deleteProjectButton = document.querySelectorAll('.btn-project-delete');\n        deleteProjectButton.forEach((element) => element.addEventListener('click', (e) => {\n            let projectName = e.target.parentNode.parentNode.textContent;\n            for (let i = 0; i < projectList.length; i++)\n            {\n                if (projectName === projectList[i].title)\n                {\n                    projectList.deleteProject(i);\n                    console.log(projectList);\n                    clearUserProjects();\n                    renderProjectList();          \n                }         \n            }\n        }))\n    }\n\n    function setActiveProject() {\n        const activeProject = document.querySelectorAll('.btn-choose-project')\n        activeProject.forEach((element) => element.addEventListener('click', (e) => {\n            console.log(e.target.textContent.trim());\n        })\n    )}\n\n    function clearUserProjects() {\n        const userProjectList = document.querySelector('.projects-user');\n        userProjectList.innerHTML = '';\n    }\n\n    function renderProjectsNav(title) {\n        const userProjectList = document.querySelector('.projects-user');\n        const userProjectButton = document.createElement('button');\n        const userProjectIcon = document.createElement('i');\n        const userProjectDeleteButton = document.createElement('button');\n        const userProjectDeleteIcon = document.createElement('i');\n        const projectTitleSpan = document.createElement('span');\n\n        userProjectIcon.classList.add('fas', 'fa-list-check');\n        userProjectDeleteIcon.classList.add('fas', 'fa-times');\n        userProjectButton.classList.add('btn-project', 'btn-choose-project');\n        userProjectDeleteButton.classList.add('btn-project-delete');\n        projectTitleSpan.textContent = title;\n\n        userProjectDeleteButton.appendChild(userProjectDeleteIcon);\n        userProjectButton.appendChild(userProjectIcon);\n        userProjectButton.appendChild(projectTitleSpan);\n        userProjectButton.appendChild(userProjectDeleteButton);\n\n        userProjectList.appendChild(userProjectButton);\n\n        deleteProject();\n        return userProjectList;\n    }\n\n})();\n\n\n\n\nclass Project {\n    constructor(title){\n        this.title = title;\n        this.tasksList = [];\n    }\n\n    getTitle() {\n        return this.title;\n    }\n\n}\n\nlet projectList = [];\n\n\nprojectList.addProject = (title) => {\n    projectList.push(new Project(title));\n};\n\nprojectList.deleteProject = (position) => {\n    projectList.splice(position, 1);\n};\n\nprojectList.contains = (projectName) => {\n    return projectList.some((project) => project.title === projectName)\n}\n\nprojectList.addProject('Your Tasks');\nprojectList.addProject('Today');\nprojectList.addProject('This week');\nconsole.log(projectList);\n\nconst test = new Project('New Project');\n\nconsole.log(test.getTitle());\n\n\n\n//     addTask(title, info, dueDate, priority) { \n//         this.tasksList.push(new Tasks(title, info, dueDate, priority));\n//     }\n// }\n\n// class Tasks {\n//     constructor(title, info, dueDate, priority) {\n//         this.title = title;\n//         this.info = info;\n//         this.dueDate = dueDate;\n//         this.priority = priority;\n//         this.completed = false;\n//     }\n\n//     toggleCompleted() {\n//         this.completed === false ? this.completed = true : this.completed = false;\n//     }\n\n//     editTask(key, value) {\n//         this[key] = value;\n//     }\n// }\n\n\n// export default Tasks;\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
