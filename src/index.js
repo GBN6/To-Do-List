82067de0f3e014fa582ae002ff423a45d78636ee
GBN6 <czesiox8@gmail.com>
2022-07-13 16:27:19 +0200

    Create new task form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //     DOM
-const createProject = (() => {
+const UI = (() => {
     setActiveProject();
     const createNewProjectButton = document.querySelector('.btn-project-user');
 
-    createNewProjectButton.addEventListener('click', displayProjectInput)
+    createNewProjectButton.addEventListener('click', displayProjectInput);
 
     function displayProjectInput() {
         const newProjectPopup = document.querySelector('.add-project-popup');
@@ -12,17 +12,15 @@ const createProject = (() => {
         addNewProject.addEventListener('click', addProject)
         newProjectPopup.classList.add('active');
         createNewProjectButton.classList.add('active');
-        closeProjectInput();
     }
 
     function closeProjectInput() {
         const newProjectPopup = document.querySelector('.add-project-popup');
-        const addNewProject = document.querySelector('.btn-add-project-popup');
         const closeProject = document.querySelector('.btn-close-poject-popup');
-        addNewProject.addEventListener('click', () => {
-            createNewProjectButton.classList.remove('active');
-            newProjectPopup.classList.remove('active')
-        })
+
+        createNewProjectButton.classList.remove('active');
+        newProjectPopup.classList.remove('active');
+
         closeProject.addEventListener('click', () => {
             createNewProjectButton.classList.remove('active');
             newProjectPopup.classList.remove('active')
@@ -46,6 +44,7 @@ const createProject = (() => {
         clearUserProjects()
         renderProjectList();
         newProjectInput.value = '';
+        closeProjectInput();
     }
 
     function renderProjectList() {
@@ -71,7 +70,8 @@ const createProject = (() => {
                     projectList.deleteProject(i);
                     console.log(projectList);
                     clearUserProjects();
-                    renderProjectList();          
+                    renderProjectList();
+                    clearTaskContainer()      
                 }         
             }
         }))
@@ -79,14 +79,70 @@ const createProject = (() => {
 
     function setActiveProject() {
         const activeProject = document.querySelectorAll('.btn-choose-project')
-        activeProject.forEach((element) => element.addEventListener('click', (e) => {
-            console.log(e.target.textContent.trim());
-        })
+        activeProject.forEach((element) => element.addEventListener('click', handleProjects)
     )}
+
+    function handleProjects(e) {
+        hideAddTaskForm();
+        const projectTitle = this.textContent.trim();
+        if (e.target.classList.contains('fa-times'))
+        {
+            deleteProject();
+            return; 
+        }
+        openProject(projectTitle, this);
+    }
+
+    function openProject(projectTitle, projectButton) {
+        const projectButtons = document.querySelectorAll('.btn-choose-project');
+        projectButtons.forEach((button) => button.classList.remove('active'));
+        projectButton.classList.add('active');
+        renderActiveProject(projectTitle);
+    }
+
+    function renderActiveProject(projectTitle) {
+        const tasksContainer = document.querySelector('.task-projects-preview');
+        const projectName = document.createElement('h1');
+        const tasksList = document.createElement('div');
+        const addTaskButton = document.createElement('button')
+        const addTaskButtonIcon = document.createElement('i');
+        const addTaskText = document.createElement('span');
+
+        clearTaskContainer()
+
+        tasksList.classList.add('task-list');
+        addTaskButton.classList.add('btn-add-task');
+        addTaskButtonIcon.classList.add('fas', 'fa-plus');
+        addTaskText.textContent = 'Add Task';
+
+        addTaskButton.appendChild(addTaskButtonIcon);
+        addTaskButton.appendChild(addTaskText);
+
+        projectName.textContent = `${projectTitle}`;
+
+        if (projectTitle === 'Today' || projectTitle === 'This week')
+        {
+            tasksContainer.appendChild(projectName);
+            tasksContainer.appendChild(tasksList);
+        }
+        else 
+        {
+            tasksContainer.appendChild(projectName);
+            tasksContainer.appendChild(tasksList);
+            tasksContainer.appendChild(addTaskButton);
+            openAddTaskForm()
+        }
+        return tasksContainer;
+    }
 
     function clearUserProjects() {
         const userProjectList = document.querySelector('.projects-user');
         userProjectList.innerHTML = '';
+    }
+
+    function clearTaskContainer() {
+        const tasksContainer = document.querySelector('.task-projects-preview');
+        tasksContainer.innerHTML = '';
     }
 
     function renderProjectsNav(title) {
@@ -111,7 +167,23 @@ const createProject = (() => {
         userProjectList.appendChild(userProjectButton);
 
         deleteProject();
+        setActiveProject();
         return userProjectList;
+    }
+
+
+    function openAddTaskForm() {
+        const openForm = document.querySelector('.btn-add-task');
+        const form = document.querySelector('.add-task-form');
+        openForm.addEventListener('click', (e) => {
+            openForm.classList.add('active');
+            form.classList.add('active');
+        })
+    }
+
+    function hideAddTaskForm() {
+        const form = document.querySelector('.add-task-form');
+        form.classList.remove('active');
     }
 
 })();
@@ -125,15 +197,37 @@ class Project {
         this.tasksList = [];
     }
 
+    addTask(title, description, dueDate, priority) {
+        this.tasksList.push(new Tasks(title, description, dueDate, priority));
+    }
+
     getTitle() {
         return this.title;
     }
 
 }
 
+class Tasks {
+    constructor(title, info, dueDate, priority) {
+        this.title = title;
+        this.info = info;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+    
+    toggleCompleted() {
+        this.completed === false ? this.completed = true : this.completed = false;
+    }
+    
+    editTask(key, value) {
+        this[key] = value;
+    }
+}
+
+
+
 let projectList = [];
-
-
 projectList.addProject = (title) => {
     projectList.push(new Project(title));
 };
@@ -145,7 +239,6 @@ projectList.deleteProject = (position) => {
 projectList.contains = (projectName) => {
     return projectList.some((project) => project.title === projectName)
 }
-
 projectList.addProject('Your Tasks');
 projectList.addProject('Today');
 projectList.addProject('This week');
